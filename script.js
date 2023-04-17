7582c825c3870aa360ccd07d0b42aff0262b4758
Xwlell <113470440+Xwlell@users.noreply.github.com>
2023-04-17 15:51:45 +0300

    Summary
    About me

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector(".thx");
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    })
+
+})
